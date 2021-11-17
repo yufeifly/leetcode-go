d5b29ebea260cca96549266a37accd0815b0edff
yufei <yufei.xiong@qq.com>
2021-11-17 19:47:18 +0800

    2021.11.17

--- q189-rotate/q189-rotate.go
+++ q189-rotate/q189-rotate.go
@@ -3,7 +3,17 @@ package q189_rotate
 func rotate(nums []int, k int) {
 	k = k % len(nums)
 
-	length := len(nums)
+	reverse(nums)
+	reverse(nums[0:k])
+	reverse(nums[k:])
+}
 
-	nums = append(nums[length-k:], nums[:length-k]...)
+func reverse(arr []int) {
+	left, right := 0, len(arr)-1
+
+	for left < right {
+		arr[left], arr[right] = arr[right], arr[left]
+		left++
+		right--
+	}
 }
